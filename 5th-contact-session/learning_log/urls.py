2016d1c7a7fbe641ff067e116e1d896164413d42
Peter Ade-Ojo <peteradeojo@outlook.com>
2022-03-30 11:32:03 +0100

    learning logs

--- 5th-contact-session/learning_log/urls.py
+++ 5th-contact-session/learning_log/urls.py
@@ -1,4 +1,4 @@
-"""multimedia URL Configuration
+"""learning_log URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
---
+++ 5th-contact-session/learning_logs/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+# Create your models here.
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Entry(models.Model):
+    # something learned about a topic
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self) -> str:
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        return self.text
